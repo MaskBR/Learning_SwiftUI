ba5598e03c3e6c43620c142f843649235b03d7ab
MaskBR <jayliu093@gmail.com>
2022-05-12 14:20:07 +0800

    Initial Commit

---
+++ Shared/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Shared
+//
+//  Created by J on 2022/5/12.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
